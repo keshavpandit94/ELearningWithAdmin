1475c22e03b2eb0b5c3b743fb11ccddc6c6a8d85
Keshav Pandit <pkeshav282@gmail.com>
2025-09-02 10:50:17 +0530

    Deploy and backend url.

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,3 +1,4 @@
 // const BACK_URL = "https://elearingplatfrom.onrender.com";
-const BACK_URL = "http://localhost:5000"
+const BACK_URL = "https://elearningwithadmin.onrender.com";
+// const BACK_URL = "http://localhost:5000"
 export default BACK_URL;
