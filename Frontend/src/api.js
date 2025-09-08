35975c69493032751758ba9568584d2f16146318
Keshav Chandra Pandit <134667288+keshavpandit94@users.noreply.github.com>
2025-09-08 17:04:09 +0530

    Update api.js

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,8 +1,6 @@
 // const BACK_URL = "https://elearingplatfrom.onrender.com";// old 
 const BACK_URL = "https://elearningwithadmin.onrender.com";
 // const BACK_URL = "http://localhost:5000"
-<<<<<<< HEAD
+
 export default BACK_URL;
-=======
-export default BACK_URL;
->>>>>>> d447a2d2ccf0916c4de4dffffffa6561b5b414df
+
